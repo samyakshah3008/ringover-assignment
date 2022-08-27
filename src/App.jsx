c471b70f4fcdb4785197b44ac215ec65a9e70ad6
Samyak Shah <samyakshah3008@gmail.com>
2022-08-28 00:29:30 +0530

    FEAT: team page integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ContactPage from "./pages/contact/ContactPage";
+import TeamPage from "./pages/team/TeamPage";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<ContactPage />} />
+        <Route path="/team" element={<TeamPage />} />
       </Routes>
     </div>
   );
