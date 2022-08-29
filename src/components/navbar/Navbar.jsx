7674f2d2eb3369142254f7c6395b5eaebfb277b0
Samyak Shah <samyakshah3008@gmail.com>
2022-08-29 20:44:06 +0530

    Path add for navigation

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import "./navbar.css";
 import websiteLogo from "../../assets/images/websitelogo.png";
 import * as FiIcons from "react-icons/fi";
-import { useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-  let navigate = useNavigate();
   return (
     <>
       <nav className="navbar-container">
         <img className="logo" alt="website logo" src={websiteLogo} />
         <div className="nav-pills-container">
           <div>HOME</div>
-          <div>THE JOURNEY</div>
-          <div onClick={() => navigate("/team")}>TEAM</div>
-          <div onClick={() => navigate("/store")}>STORE</div>
-          <div>CONTACT</div>
+          <NavLink className="individual-component" to="/journey">
+            JOURNEY
+          </NavLink>
+          <NavLink className="individual-component" to="/team">
+            TEAM
+          </NavLink>
+          <NavLink className="individual-component" to="/">
+            STORE
+          </NavLink>
+          <NavLink className="individual-component" to="/contact">
+            CONTACT
+          </NavLink>
         </div>
         <div className="user-profile-pill">
           <FiIcons.FiUser className="logo" />
