11a8a5e1862a2e94912ecabdc3cf8c7d38fcfd61
Samyak Shah <samyakshah3008@gmail.com>
2022-08-29 20:42:35 +0530

    Tree updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./App";
+import { CartProvider } from "./contexts/cart-context";
+import { FilterProvider } from "./contexts/filter-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
